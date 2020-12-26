b43a4abcf40ab2b51640f0f32cc25346515ddfe6
Barbara 4bes <43912004+Ba4bes@users.noreply.github.com>
2020-12-26 16:22:28 +0100

    update to version 0.2.0 (#4)

---
+++ CHANGELOG.md
@@ -0,0 +1,33 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.2.0] - 2020-12-26
+
+### Added
+
+- Support for multiple attachments
+- Verbose output
+- Added details to psdfile
+
+## [0.0.1] - 2019-07-26
+
+### Added
+
+- initial commit
+
+---
+
+<small>
+Cheatcheet:
+
+**Added** for new features.  
+**Changed** for changes in existing functionality.  
+**Deprecated** for soon-to-be removed features.  
+**Removed** for now removed features.  
+**Fixed** for any bug fixes.  
+**Security** in case of vulnerabilities.  
+</small>
--- PSSendGrid.psm1
+++ PSSendGrid.psm1
@@ -22,11 +22,14 @@ Function Send-PSSendGridMail {
     .PARAMETER Token
     SendGrid token for the API
     .PARAMETER AttachmentPath
-    Path to file that needs to be attached
+    Path to file(s) that needs to be attached.
+    This can be a single string or an array of strings
     .PARAMETER AttachmentDisposition
     Attachment or Inline. Use inline to add image to HTML body
     .PARAMETER AttachmentID
-    AttachmentID for inline attachment, to refer to from the HTML
+    AttachmentID(s) for inline attachment, to refer to from the HTML.
+    This can be a single string or an array of strings
+    For multiple Attachments, the IDs should be in the same order as the attachmentPaths
     .EXAMPLE
     $Parameters = @{
         FromAddress     = "example@example.com"
@@ -91,57 +94,76 @@ Function Send-PSSendGridMail {
         [string]$Token,
         [parameter()]
         [ValidateScript( { Test-Path $_ })]
-        [string]$AttachmentPath,
+        [string[]]$AttachmentPath,
         [parameter()]
         [ValidateSet('attachment', 'inline')]
         [string]$AttachmentDisposition = "attachment",
         [parameter()]
-        [string]$AttachmentID
+        [string[]]$AttachmentID
 
     )
+    Write-Verbose "Starting Function Send-PSSendGridMail"
     # Set body as HTML or as text
     if (-not[string]::IsNullOrEmpty($BodyAsHTML)) {
         $MailbodyType = 'text/HTML'
         $MailbodyValue = $BodyAsHTML
+        Write-Verbose "Found BodyAsHTML parameter, Type is set to text/HTML"
     }
     else {
         $MailbodyType = 'text/plain'
         $MailBodyValue = $Body
+        Write-Verbose "Found no BodyAsHTML parameter, Type is set to text/plain"
     }
-    # Check for attachtements. If they are present, convert to Base64
+    # Check for attachments. If they are present, convert to Base64
     if ($AttachmentPath) {
-        try {
-            if ($PSVersionTable.PSVersion.Major -lt 6) {
-                $EncodedAttachment = [convert]::ToBase64String((get-content $Attachmentpath -encoding byte))
+        Write-Verbose "AttachmentPath parameter was used"
+        $AllAttachments = @()
+        foreach ($Attachment in $AttachmentPath) {
+            try {
+                if ($PSVersionTable.PSVersion.Major -lt 6) {
+                    $EncodedAttachment = [convert]::ToBase64String((Get-Content $Attachment -encoding byte))
+                }
+                else {
+                    $AttachmentContent = Get-Content -Path $Attachment -AsByteStream -ErrorAction Stop
+
+                    $EncodedAttachment = [convert]::ToBase64String($AttachmentContent)
+                }
+                Write-Verbose "Attachment found at $Attachment has been encoded"
+            }
+            Catch {
+
+                Throw "Could not convert file $Attachment"
+            }
+
+
+            # Get the extension for the attachment type
+            $AttachmentExtension = $Attachment.Split('.')[-1]
+            Switch ($IsWindows) {
+                $true { $AttachmentName = $Attachment.Split('\')[-1] }
+                $false { $AttachmentName = $Attachment.Split('/')[-1] }
+                $Null { $AttachmentName = $Attachment.Split('\')[-1] }
+                default {
+                    Throw "Could not work with attachmentpath"
+                }
+            }
+            Write-Verbose "AttachmentExtension: $AttachmentExtension"
+            $ImageExtensions = @("jpg", "png", "gif")
+            if ($ImageExtensions -contains $AttachmentExtension) {
+                $Type = "image/$AttachmentExtension"
+                Write-Verbose "Attachment is an image, type set to $Type"
             }
             else {
-                $AttachmentContent = Get-Content -Path $AttachmentPath -AsByteStream -ErrorAction Stop
-
-                $EncodedAttachment = [convert]::ToBase64String($AttachmentContent)
+                $Type = "Application/$AttachmentExtension"
+                Write-Verbose "Attachment is not an image, type set to $Type"
             }
-        }
-        Catch {
-
-            Throw "Could not convert file $AttachmentPath"
-        }
-
-        # Get the extension for the attachment type
-        $AttachmentExtension = $AttachmentPath.Split('.')[-1]
-        Switch ($IsWindows) {
-            $true { $AttachmentName = $AttachmentPath.Split('\')[-1] }
-            $false { $AttachmentName = $AttachmentPath.Split('/')[-1] }
-            $Null { $AttachmentName = $AttachmentPath.Split('\')[-1] }
-            default {
-                Throw "Could not work with attachmentpath"
+            $Attachmentobject = [pscustomObject]@{
+                AttachmentName    = $AttachmentName
+                EncodedAttachment = $EncodedAttachment
+                AttachmentType    = $Type
             }
+            $AllAttachments += $Attachmentobject
         }
-        $ImageExtensions = @("jpg", "png", "gif")
-        if ($ImageExtensions -contains $AttachmentExtension) {
-            $Type = "image/$AttachmentExtension"
-        }
-        else {
-            $Type = "Application/$AttachmentExtension"
-        }
+        Write-Verbose "$($AllAttachments.Count) attachments have been processed"
     }
 
     # Create a body to send to the API
@@ -168,18 +190,39 @@ Function Send-PSSendGridMail {
             "name"  = $FromName
         }
     }
+    $Verbose = @"
+    ToEmail: $($SendGridBody.personalizations.To.email)
+    ToName: $($SendGridBody.personalizations.To.name)
+    FromEmail: $($SendGridBody.from.email)
+    FromName: $($SendGridBody.from.name)
+    Subject: $($SendGridBody.personalizations.subject)
+    ContentBody: $($SendGridBody.content.value)
+    ContentType: $( $SendGridBody.content.type)
+"@
+    Write-Verbose "Body send to API:"
+    Write-Verbose $Verbose
     # Add attachments to body if they are present
     if ($AttachmentPath) {
-        $Attachments = @(
-            @{
-                content     = $EncodedAttachment
-                filename    = $AttachmentName
-                type        = $Type
+        $Attachments = @()
+        $i = 0
+        foreach ($Attachmentobject in $AllAttachments) {
+            Write-Verbose "Attachment is added to body"
+            $Object = @{
+                content     = $Attachmentobject.EncodedAttachment
+                filename    = $Attachmentobject.AttachmentName
+                type        = $Attachmentobject.AttachmentType
                 disposition = $AttachmentDisposition
             }
-        )
-        if ($AttachmentID) {
-            $Attachments[0].add("content_id", $AttachmentID)
+            if ($AttachmentID) {
+                # AttachmentIDs are added in the same order as the attachments
+                $Object.add("content_id", $AttachmentID[$i])
+            }
+            $Attachments += $Object
+            $i++
+            Write-Verbose "Attachment Filename: $($Object.filename)"
+            Write-Verbose "Attachment type: $($Object.type)"
+            Write-Verbose "Attachment disposition: $($object.disposition)"
+            Write-Verbose "Attachment ObjectID: $($object.content_id)"
         }
         $SendGridBody | Add-Member -MemberType NoteProperty -Name "Attachments" -Value $Attachments
     }
@@ -203,4 +246,5 @@ Function Send-PSSendGridMail {
             "Send email")) {
         Invoke-RestMethod @Parameters
     }
+    Write-Verbose "API has been called, function is done"
 }
